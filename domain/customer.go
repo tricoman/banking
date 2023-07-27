9d45b3e08394a0ee884bd5ce37af044c93bbaf6d
Arnau Biosca <arnau.biosca@olx.com>
2023-07-27 10:27:08 +0200

    added sqlx integration

--- domain/customer.go
+++ domain/customer.go
@@ -3,11 +3,11 @@ package domain
 import "github.com/tricoman/banking/errs"
 
 type Customer struct {
-	Id          string
+	Id          string `db:"customer_id"`
 	Name        string
 	City        string
 	ZipCode     string
-	DateOfBirth string
+	DateOfBirth string `db:"date_of_birth"`
 	Status      string
 }
 
